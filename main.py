f96cfffd51e658ce6de586dc41b18b1bae51c27d
mostimo_git <72213464+MosolovTimofey@users.noreply.github.com>
2022-03-17 16:45:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,8 @@ def logout():
 def main():
     name_db = 'mars_explorer.db'
     db_session.global_init(f"db/{name_db}")
-    app.run(port=5003)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
